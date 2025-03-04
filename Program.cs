a08b33b5274584fd18010d0202ef25008c006831
Astemon <113833211+Astemon@users.noreply.github.com>
2025-03-04 16:41:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Xml.Serialization;
+
+
+// See https://aka.ms/new-console-template for more information=
+String[] console = ["GBA", "DS", "3DS", "heartgold", "ryujinx", "switch", "sudachi", "hcr","showdown"];
+Random randonConsole = new Random();
+int rndConsole = randonConsole.Next(console.Length);
+Console.WriteLine(console[rndConsole]);
+Console.ReadLine();
